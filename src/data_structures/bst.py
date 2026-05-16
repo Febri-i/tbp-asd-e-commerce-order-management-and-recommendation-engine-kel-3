e907f2646f8066125a4761cbc5c7db1944a54db9
faiznabiel <faiznabiel.2025@student.uny.ac.id>
2026-05-16 20:10:43 +0700

    feat(bst): implementasi class BSTNode

--- src/data_structures/bst.py
+++ src/data_structures/bst.py
@@ -1,1 +1,9 @@
 
+
+class BSTNode:
+    __slots__ = 'produk', 'left', 'right', 'parent'
+    def __init__(self, produk: Produk, parent: Optional['BSTNode'] = None):
+        self.produk = produk
+        self.left: Optional['BSTNode'] = None
+        self.right: Optional['BSTNode'] = None
+        self.parent: Optional['BSTNode'] = parent
