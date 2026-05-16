b747d5f5dbed3fc3e987bf720fc3e868b6d3e648
faiznabiel <faiznabiel.2025@student.uny.ac.id>
2026-05-16 20:17:56 +0700

    feat(bst): implementasi class BSTKatalog, def __init__ dan def __len__

--- src/data_structures/bst.py
+++ src/data_structures/bst.py
@@ -8,3 +8,12 @@ class BSTNode:
         self.left: Optional['BSTNode'] = None
         self.right: Optional['BSTNode'] = None
         self.parent: Optional['BSTNode'] = parent
+
+class BSTKatalog:
+    def __init__(self):
+        self.root: Optional[BSTNode] = None
+        self._size = 0
+
+    def __len__(self) -> int:
+        """Big-O = O(1)"""
+        return self._size
