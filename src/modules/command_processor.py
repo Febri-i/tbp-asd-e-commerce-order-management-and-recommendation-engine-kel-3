69efe341cd2628dc1f959e686994c8341f3e3fe3
faiznabiel <faiznabiel.2025@student.uny.ac.id>
2026-05-17 20:17:25 +0700

    refactor(cli): memperbaiki sistem import

--- src/modules/command_processor.py
+++ src/modules/command_processor.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from enum import Enum
 import numpy as np, time, random
 from typing import Dict, List, cast
