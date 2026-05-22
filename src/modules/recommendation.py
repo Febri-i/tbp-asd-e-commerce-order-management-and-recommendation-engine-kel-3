d6caeb02ba32f6bad435ffb84da5511d0c643b9a
faiznabiel <faiznabiel.2025@student.uny.ac.id>
2026-05-22 23:58:56 +0700

    fix(dev): memperbaiki import pada recommendation.py

--- src/modules/recommendation.py
+++ src/modules/recommendation.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from typing import List, Dict, Tuple
 from data_structures.linked_list import Queue
 
