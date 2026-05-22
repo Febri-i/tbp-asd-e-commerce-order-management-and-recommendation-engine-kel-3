b7e793fdf863c340b43aa688a25fd3fa410980a9
faiznabiel <faiznabiel.2025@student.uny.ac.id>
2026-05-22 20:41:04 +0700

    fix(dev): memperbaiki bagian import pada stack.py

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from src.data_structures.linked_list import LLNode
 from typing import Optional, Any
 
